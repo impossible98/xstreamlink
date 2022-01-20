d9cf41ff366823d650c4580a86a3aeb1ff0c7813
Chris <42108459+impossible98@users.noreply.github.com>
2022-01-21 00:51:25 +0800

    chore(scripts): update all

--- scripts/all.js
+++ scripts/all.js
@@ -1,19 +1,33 @@
 const fs = require('fs/promises');
+const path = require('path');
+
+class Color {
+    static green(word) {
+        return `\x1B[32m${word}\x1B[0m`;
+    }
+    static red(word) {
+        return `\x1B[31m${word}\x1B[0m`;
+    }
+
+    static yellow(word) {
+        return `\x1B[33m${word}\x1B[0m`;
+    }
+}
 
 class All {
     async getConstants() {
-        const data = await fs.readFile('Makefile', 'UTF-8');
-        let appName = data.match(/APP_NAME := (.*)/)[1];
-        let version = data.match(/APP_VERSION := (.*)/)[1];
-        let constants = [appName, version];
+        const makefilePath = path.resolve(__dirname, '..', 'Makefile');
+        const data = await fs.readFile(makefilePath, 'UTF-8');
+        const appName = data.match(/APP_NAME := (.*)/)[1];
+        const version = data.match(/APP_VERSION := (.*)/)[1];
 
-        return constants;
+        return [appName, version];
     }
 
     async print() {
         const value = await this.getConstants();
-        console.log('Building ' + value[0] + ' ' + value[1]);
-        console.log('Show help in `make help`');
+        console.log(`Building ${Color.green(value[0])} ${Color.yellow(value[1])}`);
+        console.log(`Show help in \`${Color.red('make help')}\``);
     }
 }
 
